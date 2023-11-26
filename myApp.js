909b813ba406cd68ca69e253ea04b89f243fac56
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-26 15:51:02 +0530

    test 4

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const express = require("express");
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
