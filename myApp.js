44b6cf3c712a447c677a926ca03777d8d1f4506a
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-28 10:28:16 +0530

    mangoose update t13

--- myApp.js
+++ myApp.js
@@ -32,12 +32,16 @@ const createAndSavePerson = (done) => {
   });
 
   console.log("Variable starts...");
-  aruu.save(function (err, data) {
-    if (err) return console.log(err);
-    console.log("Saved");
-    console.log(data);
-    done(null, data);
-  });
+  aruu
+    .save()
+    .then((data) => {
+      console.log("Saved");
+      console.log(data);
+      done(null, data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
--- server.js
+++ server.js
@@ -84,7 +84,9 @@ router.post("/mongoose-model", function (req, res, next) {
 const createPerson = require("./myApp.js").createAndSavePerson;
 router.get("/create-and-save-person", function (req, res, next) {
   // in case of incorrect function use wait timeout then respond
+  console.log("bacdsg");
   res.send("ABCD");
+  next();
   // let t = setTimeout(() => {
   //   next({ message: "timeout" });
   // }, TIMEOUT);
