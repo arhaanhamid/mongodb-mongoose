367e757b26e6055756d61161ff88af37029ecfc5
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-28 09:32:20 +0530

    mangoose update t2

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
+// , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB Atlas");
